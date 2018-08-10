4c4f25eec445315aa96ae65fe08bf4227c9c0c86
Amir Reza <arg7971@gmail.com>
2018-08-11 00:58:15 +0430

    added layout for the app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Wrapper from '../../HOC/Wrapper';
+import classes from './Layout.css'
 
 const layout = (props) => {
     return (
         <Wrapper>
             <div>Toolbar, Side Drawer, BackDrop</div>
-            <main>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Wrapper>
